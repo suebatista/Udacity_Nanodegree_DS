79ea46f2b38e8053a398a1dd938206b9dfe31185
Suelen Cristina <suelen.cristina.batista@gmail.com>
2021-01-29 01:50:49 -0300

    Add files via upload

---
+++ projects/Project 2 - Disaster Response Pipeline/models/train_classifier.py
@@ -0,0 +1,143 @@
+# import libraries
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+import nltk
+nltk.download(['punkt', 'wordnet'])
+from nltk.tokenize import word_tokenize
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import re
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn.neighbors import KNeighborsClassifier
+import pickle
+
+def load_database(database_filepath):
+    '''
+    Load SQL database
+    Args:
+        database_filepath - Filepath used for importing the database     
+    Returns:
+        Returns the following variables:
+        X - Returns the input features.  Specifically, this is returning the messages column from the dataset
+        Y - Returns the categories of the dataset.  This will be used for classification based off of the input X
+        y.keys - Just returning the columns of the Y columns
+    '''
+    engine = create_engine('sqlite:///data/DisasterResponse.db')
+    df =  pd.read_sql_table('DisasterResponse', engine)
+    X = df.message.values
+    y = df.iloc[:,5:]
+    return X, y, y.keys()
+
+def tokenize(text):
+    '''
+    INPUT 
+        text: Text to be processed   
+    OUTPUT
+        Returns a processed text variable that was tokenized, lower cased, stripped, and lemmatized
+    '''
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+def build_model(X_train,y_train):
+    '''
+    INPUT 
+        X_Train: Training features for use by GridSearchCV
+        y_train: Training labels for use by GridSearchCV
+    OUTPUT
+        Returns a pipeline model that has gone through tokenization, count vectorization, 
+        TFIDTransofmration and created into a ML model
+    '''
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    parameters = {  
+        'clf__estimator__min_samples_split': [2, 4],
+        #'clf__estimator__max_features': ['log2', 'auto', 'sqrt', None],
+        #'clf__estimator__criterion': ['gini', 'entropy'],
+        #'clf__estimator__max_depth': [None, 25, 50, 100, 150, 200],
+    }
+    cv = GridSearchCV(estimator=pipeline, param_grid=parameters)
+    cv.fit(X_train,y_train)
+    return cv
+
+def evaluate_model(pipeline, X_test, Y_test, category_names):
+    '''
+    INPUT 
+        pipeline: The model that is to be evaluated
+        X_test: Input features, testing set
+        y_test: Label features, testing set
+        category_names: List of the categories 
+    OUTPUT
+        This method does nto specifically return any data to its calling method.
+        However, it prints out the precision, recall and f1-score
+    '''
+    # predict on test data
+    y_pred = pipeline.predict(X_test)
+    print(classification_report(Y_test, y_pred, target_names=Y_test.keys()))
+
+def save_model(model, model_filepath):
+    '''
+    Saves the model to disk
+    INPUT 
+        model: The model to be saved
+        model_filepath: Filepath for where the model is to be saved
+    OUTPUT
+        While there is no specific item that is returned to its calling method, this method will save the model as a pickle file.
+    '''    
+    temp_pickle = open(model_filepath, 'wb')
+    pickle.dump(model, temp_pickle)
+    temp_pickle.close()
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_database(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model(X_train,Y_train)
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+        
+        
+        ###WILL NEED TO CXLEAN THIS UP
+        print('TYPE OF MODEL')
+        print(type(model))
+        
+        
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
